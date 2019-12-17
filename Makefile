b5bb538bc051a5be8949947804ccb8c7e7d582c6
edgar-badalyan <edgar.badalyan1999@gmail.com>
2019-12-17 11:45:56 +0100

    get vectors back!

--- Makefile
+++ Makefile
@@ -19,4 +19,12 @@ main.o: main.cpp pinReader.h psqReader.h phrReader.h
 smithWaterman.o: smithWaterman.cpp smithWaterman.h
 	g++ -c smithWaterman.cpp -$(CCFLAGS)
 
-
+smithWaterman3.o: smithWaterman3.cpp smithWaterman2.h
+	g++ -c smithWaterman3.cpp -$(CCFLAGS)
+	
+psqReader2.o: psqReader2.cpp psqReader.h smithWaterman2.h
+	g++ -c psqReader2.cpp -$(CCFLAGS)
+	
+main2: main.o smithWaterman3.o psqReader2.o phrReader.o pinReader.o 
+	g++ main.o smithWaterman3.o psqReader2.o phrReader.o pinReader.o -o main2 -$(CCFLAGS)
+	rm *.o
---
+++ smithWaterman2.h
@@ -0,0 +1,27 @@
+#ifndef SMITH_WATERMAN_H
+#define SMITH_WATERMAN_H
+
+#include <string>
+#include <vector>
+
+class smithWaterman {
+
+public:
+    smithWaterman();
+    void read_blosum();
+    void algo(std::vector<int> seq1, std::vector<int> seq2);
+    void set_blosum(std::string file_name) { blosum_file = file_name; }
+    void set_openPenalty(int penalty) { openPenalty = penalty; }
+    void set_extPenalty(int penalty) { extPenalty = penalty; }
+
+private:
+    int residueScores[300];
+    int openPenalty;
+    int extPenalty;
+    std::string blosum_file;
+
+    int residue_index(int residue);
+    int residues_to_pass(int line_number);
+    int get_index(int res1, int res2);
+};
+#endif

6c83801245f7f8076606af887dec708f58f48e7e
ulb-be-rkhazoum <rkhazoum@ulb.ac.be>
2019-12-18 16:12:09 +0100

    failed merge..

--- smithWaterman.cpp
+++ smithWaterman.cpp
@@ -1,8 +1,4 @@
 #include "smithWaterman.h"
-<<<<<<< HEAD
-
-=======
->>>>>>> master2
 #include <iostream>
 #include <map>
 #include <fstream>
@@ -13,39 +9,6 @@ using namespace std;
 #define min(a, b) (a < b? a : b)
 #define max(a, b) (a > b? a : b)
 
-<<<<<<< HEAD
-smithWaterman::smithWaterman(string file_name) {
-        read_blosum(file_name);
-}
-
-int residueScores[300];
-std::map<char, int> residues = {
-    {'A', 0},
-    {'R', 1},
-    {'N', 2},
-    {'D', 3},
-    {'C', 4},
-    {'Q', 5},
-    {'E', 6},
-    {'G', 7},
-    {'H', 8},
-    {'I', 9},
-    {'L', 10},
-    {'K', 11},
-    {'M', 12},
-    {'F', 13},
-    {'P', 14},
-    {'S', 15},
-    {'T', 16},
-    {'W', 17},
-    {'Y', 18},
-    {'V', 19},
-    {'B', 20},
-    {'Z', 21},
-    {'X', 22},
-    {'*', 23},
-};
-=======
 smithWaterman::smithWaterman(){
     openPenalty = 11;
     extPenalty = 1;
@@ -84,7 +47,6 @@ int smithWaterman::residue_index(int residue){
     }
     return index;
 }
->>>>>>> master2
 
 int smithWaterman::residues_to_pass(int line_number){
     if (line_number <= 0){
@@ -94,16 +56,6 @@ int smithWaterman::residues_to_pass(int line_number){
     }
 }
 
-<<<<<<< HEAD
-int smithWaterman::get_index(char res1, char res2){
-    int ind1 = residues[res1];
-    int ind2 = residues[res2];
-    return residues_to_pass(min(ind1, ind2)) + max(ind1, ind2) - min(ind1, ind2);
-}
-
-void smithWaterman::read_blosum(string file_name){
-    ifstream file(file_name);
-=======
 int smithWaterman::get_index(int res1, int res2){
     int ind1 = residue_index(res1);
     int ind2 = residue_index(res2);
@@ -112,7 +64,6 @@ int smithWaterman::get_index(int res1, int res2){
 
 void smithWaterman::read_blosum(){
     ifstream file(blosum_file);
->>>>>>> master2
     if(file.is_open()){
         string line;
         int index=0;
@@ -139,36 +90,6 @@ void smithWaterman::read_blosum(){
     }
 }
 
-<<<<<<< HEAD
-void smithWaterman::run(string seq1, string seq2, double openPenalty, double extPenalty) {
-
-    double QPenalty = openPenalty + extPenalty,
-           RPenalty = extPenalty;
-    double maxScore = 0.0;
-
-    seq1 = "0" + seq1;
-    seq2 = "0" + seq2;
-
-    // création de la matrice
-    vector<vector <double>> HMatrix(1+seq1.length(), std::vector<double>(1+seq2.length(), 0.0)),
-                            EMatrix(1+seq1.length(), std::vector<double>(1+seq2.length(), 0.0)),
-                            FMatrix(1+seq1.length(), std::vector<double>(1+seq2.length(), 0.0));
-
-    for(int y = 1; y < seq1.length(); y++){
-        for(int x = 1; x < seq2.length(); x++){
-            double maxF = max(HMatrix[y][x-1] - QPenalty, FMatrix[y][x-1] - RPenalty);
-            FMatrix[y][x] = max(maxF,0);
-            double maxE = max(HMatrix[y-1][x] - QPenalty, EMatrix[y-1][x] - RPenalty);
-            EMatrix[y][x] = max(maxE,0);
-            double maxH_1 = max(HMatrix[y-1][x-1] + residueScores[get_index(seq1[y], seq2[x])] ,0.0); double maxH_2 = max(maxF, maxE);
-            HMatrix[y][x] = max(maxH_1, maxH_2);
-            if(HMatrix[y][x] > maxScore){
-                maxScore = HMatrix[y][x];
-            }
-        }
-    }
-    cout  << floor((maxScore*0.267 + 3.34)/log(2))<< endl;
-=======
 int smithWaterman::algo(const vector<int> & seq1, const vector<int> & seq2) {
     double QPenalty = openPenalty + extPenalty,
            RPenalty = extPenalty;
@@ -202,5 +123,4 @@ int smithWaterman::algo(const vector<int> & seq1, const vector<int> & seq2) {
         return  floor((max*0.267 + 3.34)/log(2));
     }
 
->>>>>>> master2
 }
--- smithWaterman.h
+++ smithWaterman.h
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-
-#include <string>
-using namespace std;
-
-class smithWaterman {
-public:
-    smithWaterman(string file_name = "BLOSUM62");
-
-    int residues_to_pass(int line_number);
-    int get_index(char res1, char res2);
-    void read_blosum(string file_name);
-    void run(string seq1, string seq2, double openPenalty = 11.0, double extPenalty = 1.0);
-private:
-=======
 #include <string>
 #include <vector>
 
@@ -36,5 +21,4 @@ private:
     int residue_index(int residue);
     int residues_to_pass(int line_number);
     int get_index(int res1, int res2);
->>>>>>> master2
 };
